b3eb24110c4610a5b9dab11fcb8595a2ff429bd9
abxst <hainthvl@gmail.com>
2025-09-22 10:47:07 +0700

    fix cors

--- src/cors.ts
+++ src/cors.ts
@@ -15,7 +15,7 @@ export function withCors(response: Response, request: Request): Response {
 	const headers = new Headers(response.headers);
 	headers.set('Access-Control-Allow-Origin', allowOrigin);
 	headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-	headers.set('Access-Control-Allow-Headers', '*');
+	headers.set('Access-Control-Allow-Headers', 'Content-Type, *');
 	headers.set('Access-Control-Allow-Credentials', 'true');
 	headers.set('Access-Control-Max-Age', '86400');
 	return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
@@ -27,7 +27,7 @@ export function preflightCors(request: Request): Response {
 	const headers: HeadersInit = {
 		'Access-Control-Allow-Origin': allowOrigin,
 		'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-		'Access-Control-Allow-Headers': '*',
+		'Access-Control-Allow-Headers': 'Content-Type, *',
 		'Access-Control-Allow-Credentials': 'true',
 		'Access-Control-Max-Age': '86400',
 	};
